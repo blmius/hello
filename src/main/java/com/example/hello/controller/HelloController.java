27acba4b718712ad18d20ca7d5669eac89ff40ba
blmius <blmius@163.com>
2018-11-05 15:03:13 +0800

    test1

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
     }
 }
